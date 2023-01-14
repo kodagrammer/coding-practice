78670748976c24d92403eb19dc58542d72b033c4
kodagrammer <dyko180801@gmail.com>
2023-01-14 09:35:40 +0900

    Programmers(Level.1) - 신고결과받기 For Kakao

---
+++ coding-practice/CodingTest/src/java/codingtest/level1/GetReportResult.java
@@ -0,0 +1,72 @@
+package java.codingtest.level1;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/*################################
+ * 플랫폼: 프로그래머스
+ * 레벨: Level 1
+ * 문제유형: 2022 KAKAO BLIND RECRUITMENT
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/92334
+ * ################################
+ */
+public class GetReportResult {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+
+        Map<String, ReportInfo> reportedInfo = getReportInfo(report);
+        List<String> idList = java.util.Arrays.asList(id_list);
+
+        for(ReportInfo info : reportedInfo.values()) {
+            if (info.getReportedCnt() >= k){
+                List<String> reportingUser = info.getReportingUser();
+
+                for (String reportingUserId : reportingUser){
+                    int idIdx = idList.indexOf(reportingUserId);
+                    answer[idIdx]++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private Map<String, ReportInfo> getReportInfo(String[] report) {
+        Map<String, ReportInfo> reportInfoMap = new HashMap<>();
+
+        for(String item : Arrays.stream(report).distinct().collect(Collectors.toList())) {
+            String reportingUser = item.split(" ")[0];
+            String reportedUser = item.split(" ")[1];
+
+            if (!reportInfoMap.containsKey(reportedUser)){
+                ReportInfo reportInfo = new ReportInfo(reportedUser);
+                reportInfo.reported(reportingUser);
+
+                reportInfoMap.put(reportedUser, reportInfo);
+            } else {
+                reportInfoMap.get(reportedUser).reported(reportingUser);
+            }
+        }
+
+        return reportInfoMap;
+    }
+
+
+    private class ReportInfo {
+        private String id;
+        private List<String> reportingUser;
+
+        public ReportInfo (String id) {
+            this.id = id;
+            this.reportingUser = new ArrayList<>();
+        }
+
+        public String getId() { return this.id; }
+        public int getReportedCnt() { return this.reportingUser.size(); }
+        public List<String> getReportingUser() { return this.reportingUser; }
+
+        public void reported(String reportingUserId){
+            this.reportingUser.add(reportingUserId);
+        }
+    }
+}
